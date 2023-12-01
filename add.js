d7573d647795f3e2bbe44e91ceb40338ae213cba
nanasi-1 <test_email>
2023-12-01 19:38:54 +0900

    cssは諦めた

--- add.js
+++ add.js
@@ -12,16 +12,25 @@ window.addEventListener('load', () => {
 
     /** @type {Document} */
     const ifrDocument = document.querySelector('[aria-label="教材モーダル"]>iframe').contentDocument;
-    appendBookmarkBtn(ifrDocument);
 
     // CSSを挿入
     const link = strToElement(`<link rel="stylesheet" href="${chrome.runtime.getURL('style.css')}"></link>`);
-    document.body.append(link);
+    document.head.append(link);
+
+    appendBookmarkBtn(ifrDocument);
 });
 
 /** ブックマークのボタンを追加する関数 @param {Document} doc */
 function appendBookmarkBtn(doc) {
-    const btn = strToElement('<button id="bookmark-btn" class="u-button type-primary question">ブックマーク</button>');
+    const btn = strToElement('<button id="bookmark-btn" class="u-button type-primary">ブックマーク</button>');
+    btn.style.position = 'absolute';
+    btn.style.right = '130px';
+    btn.style.top = '0';
+    btn.style.marginTop = '-10px';
+    btn.style.padding = '0';
+    btn.style.lineHeight = '42px';
+    btn.style.width = '120px';
+
     const header = doc.querySelector('header');
     header.insertBefore(btn, header.querySelector('#question-btn'));
 }
